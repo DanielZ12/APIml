027552af68df93d4661f128349a8793b612772db
DanielZ12 <zambranadanieljh@gmail.com>
2022-11-04 21:08:44 -0300

    authController login terminado

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,6 +1,8 @@
 const { ROL_ADMIN, ROL_USER } = require('../constants');
 const db = require('../database/models');
-const {sign} = require("jsonwebtoken")
+const {sign} = require("jsonwebtoken");
+const { hashSync, compare } = require('bcryptjs');
+
 
 module.exports = {
     register: async (req, res) => {
@@ -8,7 +10,7 @@ module.exports = {
           const {name, surname, email, password, street, city, province} = req.body
 
           if (!email || !password) {
-            res.status(412).json({
+            return res.status(412).json({
                 ok: false,
                 status: 401
             })
@@ -17,7 +19,7 @@ module.exports = {
             name: name?.trim(),
             surname: surname?.trim(),
             email: email?.trim(),
-            password: password?.trim(),
+            password: hashSync(password?.trim(), 12),
             street: street?.trim(),
             city: city?.trim(),
             province: province?.trim(),
@@ -51,8 +53,57 @@ module.exports = {
             });
         }
     },
-    login: (req, res) => {
+    login: async(req, res) => {
+      try {
+        const { email, password } = req.body
 
+        if (!email || !password) {
+          return res.status(401).json({
+              ok: false,
+              status: 401,
+              msg: "El email y password son requeridos"
+          })
+        }
+
+        const {id, rolId, password: passwordHash} = await db.User.findOne({
+          where: {email}
+        })
+        if (!id) {
+          return res.status(404).json({
+            ok: false,
+            status: 404,
+            msg: "No existe el usuario"
+        })
+        }
+
+        const isPassValid = await compare(password, passwordHash)
+
+        if (!isPassValid) {
+          return res.status(401).json({
+            ok: false,
+            status: 401,
+            msg: "Credenciales invalidas"
+        })
+        }
+
+        const token = await sign({ id, rolId}, process.env.SECRET_KEY_JWT, {
+          expiresIn: "4h",
+        });
+
+        res.status(200).json({
+          ok: true,
+          status: 200,
+          token,
+          urlData: `${req.protocol}://${req.get('host')}${req.baseUrl}/me/${token}`
+        })
+
+      } catch (error) {
+        res.status(500).json({
+          ok:false,
+          status: 500,
+          mge: error.messege
+      });
+      }
     },
     getUserAuthenticated: (req, res) => {
         
